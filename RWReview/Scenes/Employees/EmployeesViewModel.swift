7ff9b51abc481e5f6ef5e56f4bf4014e13dd7e6f
Svyat Zubyak <svyat.zubyak@gmail.com>
2020-05-12 23:22:53 +0300

    Add EmployeesViewModel file

---
+++ RWReview/Scenes/Employees/EmployeesViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  EmployeesViewModel.swift
+//  RWReview
+//
+//  Created by Svyat Zubyak on 12.05.2020.
+//  Copyright © 2020 Svyat Zubyak. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class EmployeesViewModel: ObservableObject {
+    
+    private var employeesRepositoryProvider: EmployeesRepositoryProviderProtocol
+    var employees: [EmployeeViewModel] = []
+    
+    @Published var activeEmployees: [EmployeeViewModel] = []
+    @Published var inactiveEmployees: [EmployeeViewModel] = []
+    @Published var isLoading: Bool = false
+    @Published var alert: AlertData?
+    
+    init(employeesRepositoryProvider: EmployeesRepositoryProviderProtocol = EmployeesRepositoryProvider()) {
+        self.employeesRepositoryProvider = employeesRepositoryProvider
+    }
+    
+    func fetchEmployees() {
+        isLoading = true
+        let repository = employeesRepositoryProvider.getEmployeesRepository()
+        repository.fetchEmployees() { [weak self] result in
+            self?.isLoading = false
+            switch(result) {
+            case .success(let employees):
+                self?.employees = employees.map(EmployeeViewModel.init)
+                self?.filterEmployees()
+            case .failure(let error) :
+                self?.alert = AlertData(message: error.localizedDescription)
+            }
+        }
+    }
+    
+    private func filterEmployees() {
+        activeEmployees = employees.filter { $0.isActive }
+        inactiveEmployees = employees.filter { !$0.isActive }
+    }
+}
+
+extension EmployeesViewModel: AddEmployeeDelegate {
+    
+    func employeeCreated(_ employee: EmployeeViewModel) {
+        employees.append(employee)
+        filterEmployees()
+    }
+}
