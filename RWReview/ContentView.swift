e1a9bde6036d7d7f436b1e0761360d27fbe3f475
Svyat Zubyak <svyat.zubyak@gmail.com>
2020-05-12 23:13:37 +0300

    Initial Commit

---
+++ RWReview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RWReview
+//
+//  Created by Svyat Zubyak on 12.05.2020.
+//  Copyright © 2020 Svyat Zubyak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
